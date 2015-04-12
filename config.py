198260801cdf29483878db04ab9fa96bbdc30a81
Danny Chrastil <danny.chrastil@gmail.com>
2015-04-11 21:33:07 -0700

    Config file

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+## [LINKEDIN CREDENTIALS] ##
+# it may be preferable to use a fake
+# account to avoid account suspension
+
+linkedin = dict(
+    username = '',
+    password = '',
+)
+
+## [PROXY LIST] ##
+# Leave empty to use your own IP address
+# by using a proxy you can avoid being
+# blocked for sending too much traffic
+
+proxylist = []
+#proxylist.append('https://209.173.8.216:8080')
+
+## [MISCELLANEOUS] ##
+
+timeout = 10
+
